1b7634e667b88758c9774ed27bdcc132d77cff9b
jernejfrank <jernejfrank@gmail.com>
2025-01-15 23:15:32 +0800

    Add docker compose for pg and pgadmin

--- src/inject_csv_into_pg.py
+++ src/inject_csv_into_pg.py
@@ -4,10 +4,6 @@ import pandas as pd
 import sqlalchemy as sa
 from pandas.io.parsers.readers import TextFileReader
 from sqlalchemy import Engine, create_engine
-from tqdm import tqdm
-
-URL = "postgresql://root:root@localhost:5432/ny_taxi"
-FILE = "yellow_tripdata_2021-01.csv"
 
 
 def create_connection(url: str) -> Engine:
@@ -21,31 +17,47 @@ def create_pd_iterator_from_csv(file: str, batch: int = 100000) -> TextFileReade
     return df_iter
 
 
-def init_table(columns: pd.DataFrame, conn: Engine) -> None:
+def init_table(table_name: str, columns: pd.DataFrame, conn: Engine) -> None:
     """Creates new table in postgres db. Drops existing table."""
-    columns.to_sql(name="yellow_taxi_data", con=conn, if_exists="replace")
+    columns.to_sql(name=table_name, con=conn, if_exists="replace")
 
 
-def convert_columns_to_datetime(df: pd.DataFrame) -> pd.DataFrame:
+def convert_columns_to_datetime(df: pd.DataFrame, table_name: str) -> pd.DataFrame:
     """Converts dataframe columns to datetime."""
-    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    if "yellow" in table_name:
+        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    elif "green" in table_name:
+        df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+        df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
     return df
 
 
-def main() -> None:
+def main(table_name: str, file: str, db_url: str, reset_table: bool = False) -> None:
     """Injecting csv data into postgres db pipeline."""
-    df_iter = create_pd_iterator_from_csv(FILE)
-    conn = create_connection(URL)
-    df = next(df_iter)
+    df_iter = create_pd_iterator_from_csv(file)
+    conn = create_connection(db_url)
 
-    if not sa.inspect(conn).has_table("yellow_taxi_data"):
-        init_table(df.head(n=0), conn)
+    total_rows = 1369766  # 476386
+    current_rows = 0
+    for i, batch in enumerate(df_iter):
+        batch = convert_columns_to_datetime(batch, table_name=table_name)
 
-    for batch in tqdm(df_iter):
-        batch = convert_columns_to_datetime(batch)
-        batch.to_sql(name="yellow_taxi_data", con=conn, if_exists="append")
+        if i == 0 and (reset_table or not sa.inspect(conn).has_table(table_name)):
+            init_table(table_name, batch.head(n=0), conn)
+
+        batch.to_sql(name=table_name, con=conn, if_exists="append")
+        current_rows += batch.shape[0]
+        print(f"Processed {current_rows} / {total_rows} rows")
+
+    print()
 
 
 if __name__ == "__main__":
-    main()
+    URL = "postgresql://root:root@localhost:5432/ny_taxi"
+    FILE = "yellow_tripdata_2021-01.csv"
+    NAME = "yellow_taxi_data"
+
+    # FILE = "green_tripdata_2019-10.csv"
+    # NAME = "green_taxi_data"
+    main(table_name=NAME, file=FILE, db_url=URL, reset_table=True)
